e8a6d1802dc918b23ebf026ddb2fa4557866fd23
JDespo <44578440+JDespo@users.noreply.github.com>
2019-10-15 15:08:17 -0300

    Update funciones.c

--- funciones.c
+++ funciones.c
@@ -25,3 +25,45 @@ int pop(STR_NODO **pila)
     free(aux);
     return dato;
 }
+
+
+void ordenarPila(STR_NODO **pila)
+{
+     STR_NODO *aux = NULL;
+    STR_NODO *aux2 = NULL;
+
+        while( pila != NULL)
+        {
+            
+            int numero = pop(pila);
+            if(aux == NULL)
+            {
+                push(&aux, numero);
+                printf("\nNULL: AUX->NUM: %i\n",aux->numero);
+            }
+            else{
+                printf("\nAUX->NUM: %i\n",aux->numero);
+                printf("PILA: %i\n",numero);
+                while(aux!= NULL && numero >= aux->numero)
+                {
+                    printf("WHILE AUX->NUM: %i\n",aux->numero);
+                    push(&aux2, pop(&aux));
+                    printf("\npush aux2 -> pop aux %i\n",aux2->numero);
+                }
+                
+                push(&aux, numero);
+                printf("GUARDO NUM EN AUX\n");
+
+                while(aux2 != NULL)
+                {   
+                    printf("push aux pop aux2\n");
+                    push(&aux, pop(&aux2));
+                }
+            }
+        }
+
+        while(aux != NULL)
+        {
+            push(pila, pop(&aux));
+        }
+}
